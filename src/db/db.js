30b6e5926f5686a93141a75dc1579733b22badb7
Leeco <leeco19@qq.com>
2022-09-21 00:10:38 +0800

    update MONGODB_URI

--- src/db/db.js
+++ src/db/db.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose')
 const uri = 'mongodb://localhost:27017'
 const dbName = 'comment3'
 
+const MONGODB_URI = process.env.MONGODB_URI || `${uri}/${dbName}`
+
 // 开始链接
-mongoose.connect(`${uri}/${dbName}`)
+mongoose.connect(MONGODB_URI)
 
 // 获取连接对象
 const conn = mongoose.connection
